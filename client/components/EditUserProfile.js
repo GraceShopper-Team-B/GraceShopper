e06dcfcab8a5aa9e8e395c5fc591c4048560da5f
Esther Yoo <liletchie@gmail.com>
2022-11-09 18:17:45 -0500

    fixed bug on form

--- client/components/EditUserProfile.js
+++ client/components/EditUserProfile.js
@@ -5,7 +5,6 @@ import { updatingUser } from "../store/user";
 export class UpdateUserProfile extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props);
     this.state = {
       firstName: this.props.user.firstName,
       lastName: this.props.user.lastName,
@@ -29,46 +28,17 @@ export class UpdateUserProfile extends React.Component {
       phoneNumber: this.state.phoneNumber,
       password: this.state.password,
     });
-    this.setState({
-      firstName: this.props.user.firstName,
-      lastName: this.props.user.lastName,
-      email: this.props.user.email,
-      password: this.props.user.password,
-      phoneNumber: this.props.user.phoneNumber,
-    });
   }
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
     });
-    if (event.target.name === "firstName") {
-      if (event.target.value.length === 0) {
-        this.setState({ error: "First name must be filled." });
-      } else {
-        this.setState({ error: "" });
-      }
-    }
-
-    if (event.target.name === "lastName") {
-      if (event.target.value.length === 0) {
-        this.setState({ error: "Last name must be filled." });
-      } else {
-        this.setState({ error: "" });
-      }
-    }
-
-    if (event.target.name === "email") {
-      if (event.target.value.length === 0) {
-        this.setState({ error: "Email must be filled." });
-      } else {
-        this.setState({ error: "" });
-      }
-    }
   }
 
   render() {
     const { handleChange } = this;
     const { handleSubmit } = this;
+
     return (
       <form className="form" onSubmit={handleSubmit}>
         <h3>Update User</h3>
@@ -129,87 +99,3 @@ const mapDispatch = (dispatch) => ({
 });
 
 export default connect(mapState, mapDispatch)(UpdateUserProfile);
-
-// const UpdateUserProfile = props => {
-//   const {name, handleSubmit, handleChange, type, value, error} = props;
-//   const [Input, setInput]= useState({
-//     name: name,
-//     value: value,
-//   });
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label htmlFor="">
-//             <small>Enter New Information</small>
-//           </label>
-//           <input name= {name} value = {value} type= {type} onChange = {handleChange} />
-//         </div>
-//         <div>
-//           <button type="submit">Edit</button>
-//         </div>
-//         {error && error.response && <div> {error.response.data} </div>}
-//       </form>
-//     </div>
-//   )
-// }
-
-// const mapFirstName = state => {
-//   return {
-//     name: 'firstName',
-//     value: '',
-//     type: "text",
-//     error: state.auth.error
-//   }
-// }
-
-// const mapLastName = state => {
-//   return {
-//     name: 'lastName',
-//     value: '',
-//     type: "text",
-//     error: state.auth.error
-//   }
-// }
-// const mapEmail = state => {
-//   return {
-//     name: 'email',
-//     value: '',
-//     type: "text",
-//     error: state.auth.error
-//   }
-// }
-
-// const mapPhoneNumber = state => {
-//   return {
-//     name: 'phoneNumber',
-//     value: '',
-//     type: "text",
-//     error: state.auth.error
-//   }
-// }
-// const mapPassword = state => {
-//   return {
-//     name: 'password',
-//     value: '',
-//     type: "password",
-//     error: state.auth.error
-//   }
-// }
-
-// const mapDispatch = dispatch => {
-//   return {
-//     handleSubmit(evt) {
-//       evt.preventDefault()
-//       const userInfo = {
-//         id: state.auth.id,
-
-//       }
-//       dispatch(updatingUser(username, password, formName))
-//     },
-//     handleChange(){
-
-//     }
-//   }
-// }
