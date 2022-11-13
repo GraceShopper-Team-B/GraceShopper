bd0b6b530d9b81bbab847798da2c49e2421ff882
Naomi Brender <NaomiBBrender@gmail.com>
2022-11-13 13:50:41 -0500

    addToCart in single product

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchSingleProduct } from "../store/singleProduct";
+import { addItem } from "../store/cartItem";
 
 class SingleProduct extends React.Component {
   componentDidMount() {
@@ -11,14 +12,30 @@ class SingleProduct extends React.Component {
     }
   }
   render() {
+    const orders = this.props.auth.orders || {};
+    const order = orders[0] || [];
+    const orderId = order.id;
+    console.log("order", order);
     const product = this.props.singleProduct;
-    console.log("What's product", product);
+    // console.log("What's product", product);
     return (
       <div>
         <h1>{product.name}</h1>
         <img width="300" src={product.image} />
         <p>{product.description}</p>
         <h3>${product.price}</h3>
+
+        <button
+          type="button"
+          onClick={() =>
+            this.props.addItem({
+              productId: product.id,
+              orderId: orderId,
+            })
+          }
+        >
+          Add To Cart
+        </button>
       </div>
     );
   }
@@ -26,12 +43,14 @@ class SingleProduct extends React.Component {
 const mapState = (state) => {
   return {
     singleProduct: state.singleProduct,
+    auth: state.auth,
   };
 };
 
 const mapDispatch = (dispatch) => {
   return {
     loadSingleProduct: (id) => dispatch(fetchSingleProduct(id)),
+    addItem: (newCartItem) => dispatch(addItem(newCartItem)),
   };
 };
 
