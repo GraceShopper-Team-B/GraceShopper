b18ca6f9c40426f120ed6d232132057474c406dd
Ilana Bye <ilanagbye@gmail.com>
2022-11-16 07:47:20 -0700

    small tweek

--- client/components/UpdateProduct.js
+++ client/components/UpdateProduct.js
@@ -61,7 +61,7 @@ export class UpdateProduct extends React.Component {
     console.log("What's product", this.props);
     return (
       <div>
-        <h2>Update Product Information</h2>
+        <h2>Update {product.name} Information</h2>
         <form id="update-product" onSubmit={this.handleSubmit}>
           <label> Product Name:</label>
           <input
